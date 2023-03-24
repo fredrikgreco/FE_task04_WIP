92cd2acc1807e1a4f3c4ccb12c40cbfec19c61b6
fredrikgreco <112250686+fredrikgreco@users.noreply.github.com>
2023-03-24 06:44:20 +0100

    tests added

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,18 +2,52 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://127.0.0.1:5500/index.html');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/School and Back/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('writeable_field', async ({ page }) => {
+//   await page.goto('http://127.0.0.1:5500/index.html');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+//   await page.fill('#stop-input', 'Sexdrega');
+
+//   await page.click('#updateButton');
+
+//   const ulist = page.locator('ul > li');
+
+//   await expect(ulist).toExist();
+
+// });
+
+test('test2', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5500/');
+
+  await page.fill('#stop-input', 'Sexdrega');
+
+  await page.click('#updateButton');
+
+  await page.waitForSelector('ul');
+
+  const listOneText = await page.$eval('#listOne', el => el.textContent.trim());
+  expect(listOneText).toBe('Sexdrega%');
+
+  ;
+});
+
+test('testButton', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5500/');
+
+  await page.fill('#stop-input', 'Sexdrega');
+
+  await page.click('#addFavButton');
+
+  await page.waitForSelector('div');
+
+  const listOneText = await page.$eval('#buttonContainer', el => el.textContent.trim());
+  expect(listOneText).toBe('SexdregaX');
+
+  ;
 });
